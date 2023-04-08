dae0e0a333685adcbc0c9815c78524a5a8e1ab3f
moris344 <130246734+moris344@users.noreply.github.com>
2023-04-08 20:46:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+for(let i = 0;i<1;){
+    broj = prompt("Unesi broj");
+
+    if (broj == 5) {
+        i++;
+        alert("TAČAN ODGOVOR");
+    }else {
+        alert("POKUŽAJ PONOVO");
+    }
+}
